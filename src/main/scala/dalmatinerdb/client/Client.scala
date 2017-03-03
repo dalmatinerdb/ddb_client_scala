725892079118fc7ac3d8764fe5e86cb25cf24e73
David Cromberge <david.cromberge@dataloop.io>
2017-03-03 14:23:09 +0000

    Writes use values not double

--- src/main/scala/dalmatinerdb/client/Client.scala
+++ src/main/scala/dalmatinerdb/client/Client.scala
@@ -17,7 +17,7 @@ object Client {
 
 trait Client extends Closable {
   def query(bucket: String, metric: String, time: Long, count: Long): Future[Result]
-  def write(metric: String, time: Long, value: Double): Future[Result]
+  def write(metric: String, time: Long, value: Value): Future[Result]
   def flush(): Future[Result]
   def close(): Future[Unit]
 }
@@ -31,8 +31,8 @@ final class StdClient(val factory: ServiceFactory[Request, Result]) extends Clie
   def query(bucket: String, metricPath: String, time: Long, count: Long): Future[Result] =
     service(Query(bucket, new Metric(metricPath), time, count))
 
-  def write(metricPath: String, time: Long, value: Double): Future[Result] =
-    service(Write(new Metric(metricPath), time, FloatValue(value)))
+  def write(metricPath: String, time: Long, value: Value): Future[Result] =
+    service(Write(new Metric(metricPath), time, value))
 
   def flush(): Future[Result] =
     service(Flush)
